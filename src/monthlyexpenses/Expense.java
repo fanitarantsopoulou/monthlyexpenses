46bd2723624a9a76d578c8ec81b01ca82ddecd3a
Ethan McCue <ethan@mccue.dev>
2023-10-12 10:07:00 -0400

    Use public fields

--- src/monthlyexpenses/Expense.java
+++ src/monthlyexpenses/Expense.java
@@ -3,10 +3,10 @@ package monthlyexpenses;
 public class Expense {
 
     // Declaration of variables
-    private String month;
-    private String day;
-    private String description;
-    private double amount;
+    public final String month;
+    public final String day;
+    public final String description;
+    public final double amount;
 
     // Constructor
     public Expense(String month, String day, String description, double amount) {
@@ -16,38 +16,6 @@ public class Expense {
         this.amount = amount;
     }
 
-    // Methods
-    public String getMonth() {
-        return month;
-    }
-
-    public void setMonth(String month) {
-        this.month = month;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public void setDay(String day) {
-        this.day = day;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
 
     @Override
     public String toString() {
